2129e2c906cdfe1ae9e0c78f8094815f32469d5a
Quentin Tellier <quentin.tellier@outlook.com>
2026-09-23 14:34:57 +0200

    Update Collaborator Input Validation: In collaborator_view.py: In UpdateCollaboratorScreen: added input validation. Updated .tcss file accordingly.

--- collaborators/collaborator_view.py
+++ collaborators/collaborator_view.py
@@ -543,6 +543,9 @@ class UpdateCollaboratorScreen(Screen):
     SUB_TITLE = "UPDATE COLLABORATOR"
     CSS_PATH = "../styles/update_collaborator_screen.tcss"
 
+    # Reactive variables
+    is_form_valid: reactive[bool] = reactive(False)
+
     def __init__(
         self, collaborator_data: dict, departments: list[Department]
     ):
@@ -572,14 +575,34 @@ class UpdateCollaboratorScreen(Screen):
                     value=self.collaborator_data.get(
                         "collaborator_last_name", ""
                     ),
+                    placeholder="Doe",
                     id="collaborator_last_name",
+                    type="text",
+                    max_length=50,
+                    classes="form-input",
+                    validators=[
+                        Length(
+                            minimum=1,
+                            failure_description="Field cannot be empty.",
+                        ),
+                    ],
                 )
                 yield Label("Collaborator First Name:")
                 yield Input(
                     value=self.collaborator_data.get(
                         "collaborator_first_name", ""
                     ),
+                    placeholder="John",
                     id="collaborator_first_name",
+                    type="text",
+                    max_length=50,
+                    classes="form-input",
+                    validators=[
+                        Length(
+                            minimum=1,
+                            failure_description="Field cannot be empty.",
+                        ),
+                    ],
                 )
             with Container(
                 id="update-collaborator-department",
@@ -631,6 +654,84 @@ class UpdateCollaboratorScreen(Screen):
         collaborator_department_container.border_title = (
             "Department Selection"
         )
+
+    def watch_is_form_valid(self, is_valid: bool) -> None:
+        """Disable/enable Update button based on form input validation."""
+        update_button = self.query_one("#update", Button)
+        update_button.disabled = not is_valid
+
+    def _validate_form(self) -> None:
+        """Validates every input field of the form and
+        updates `is_form_valid` reactive variable
+        """
+        widget_inputs = [
+            self.query_one("#collaborator_last_name", Input),
+            self.query_one("#collaborator_first_name", Input),
+        ]
+
+        widget_selects = [
+            self.query_one(
+                "#update-collaborator-department-select", Select
+            ),
+        ]
+
+        # Check that all input fields are valid
+        all_inputs_valid = all(
+            widget_input.validate(widget_input.value).is_valid
+            for widget_input in widget_inputs
+        )
+        # Check that all select fields are valid
+        all_selects_valid = all(
+            not select.is_blank() for select in widget_selects
+        )
+
+        # Update reactive variable triggering watcher
+        all_valid = all_inputs_valid and all_selects_valid
+        self.is_form_valid = all_valid
+
+    @on(Input.Changed)
+    def show_input_invalid_reasons(self, event: Input.Changed) -> None:
+        """Activates on changed input"""
+        # Updating the UI to show the reasons why validation failed
+        self._validate_form()
+        input_widget = event.input
+
+        if event.validation_result.is_valid:
+            input_widget.border_subtitle = None
+        else:
+            # Get first error message from list and display it
+            error_message = event.validation_result.failure_descriptions[0]
+            input_widget.border_subtitle = error_message
+
+    @on(Input.Blurred)
+    def show_input_invalid_reasons_2(self, event: Input.Blurred) -> None:
+        """Activates on blurred (losing focus) input"""
+        # Updating the UI to show the reasons why validation failed
+        self._validate_form()
+        input_widget = event.input
+
+        if event.validation_result.is_valid:
+            input_widget.border_subtitle = None
+        else:
+            # Get first error message from list and display it
+            error_message = event.validation_result.failure_descriptions[0]
+            input_widget.border_subtitle = error_message
+
+    @on(Select.Changed)
+    def show_select_invalid_reasons(self, event: Select.Changed) -> None:
+        """Activates on changed input"""
+        # Updating the UI to show the reasons why validation failed
+        self._validate_form()
+        select_widget = event.select
+        select_current = select_widget.query_one(SelectCurrent)
+
+        if event.select.is_blank():
+            error_message = "An option must be selected."
+            select_current.set_class(True, "-invalid")
+            select_current.border_subtitle = error_message
+        else:
+            select_current.set_class(False, "-invalid")
+            select_current.border_subtitle = None
 
     @on(Button.Pressed, "#update")
     def go_update(self) -> None:
